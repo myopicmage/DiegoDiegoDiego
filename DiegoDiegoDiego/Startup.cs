f6e0d57ec8cc36eb2e1a4b0d939142805bc40c2f
kcber <kcber@DESKTOP-QR49RMK>
2016-06-21 19:08:03 -0400

    Add project files.

---
+++ DiegoDiegoDiego/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DiegoDiegoDiego.Startup))]
+namespace DiegoDiegoDiego
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
